5b3a3f52460b96a819ae6383e2f8a6f169a701c7
abudnik <budnik27@gmail.com>
2016-09-02 12:14:50 +0300

    small fixes

--- primes.cpp
+++ primes.cpp
@@ -1,12 +1,13 @@
 #include <cstdint>
 #include <cstdio>
 #include <cmath>
+#include <inttypes.h>
 
 
 const uint64_t MAX_N = 1 * 1000 * 1000 * 1000ULL;
 const uint64_t MAX_N_SQRT = sqrt(MAX_N);
 
-typedef int64_t ssize_t;
+typedef int64_t index_t;
 
 class HashTable
 {
@@ -32,7 +33,7 @@ public:
 		values[i] = v;
 	}
 
-	ssize_t search(uint64_t k) const {
+	index_t search(uint64_t k) const {
 		size_t i = k & mask;
 		while (keys[i]) {
 			if (keys[i] == k)
@@ -43,7 +44,7 @@ public:
 		return -1;
 	}
 
-	void remove(ssize_t i) {
+	void remove(index_t i) {
 		keys[i] = 0;
 
 		for (i = (i + 1) & mask; keys[i]; i = (i + 1) & mask) {
@@ -53,7 +54,7 @@ public:
 		}
 	}
 
-	uint64_t get_value(ssize_t pos) const {
+	uint64_t get_value(index_t pos) const {
 		return values[pos];
 	}
 
@@ -69,7 +70,7 @@ void FindPrimes()
 	printf("2 ");
 
 	for (uint64_t n = 3; n < MAX_N; n += 2) {
-		const ssize_t pos = ht.search(n);
+		const index_t pos = ht.search(n);
 		if (pos >= 0) {
 			const uint64_t p = ht.get_value(pos);
 			ht.remove(pos);
@@ -80,7 +81,7 @@ void FindPrimes()
 			if (n <= MAX_N_SQRT) {
 				ht.insert(n * n, n);
 			}
-			printf("%lu ", n);
+			printf("%" PRIu64 " " , n);
 		}
 	}
 }
